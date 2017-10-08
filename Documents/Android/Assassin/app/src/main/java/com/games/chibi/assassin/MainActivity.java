bc906efc319771439995093898144194b4d014e2
Jack Zhang <jackzhang1067@berkeley.edu>
2017-10-07 17:06:21 -0700

    Skeleton

--- Documents/Android/Assassin/app/src/main/java/com/games/chibi/assassin/MainActivity.java
+++ Documents/Android/Assassin/app/src/main/java/com/games/chibi/assassin/MainActivity.java
@@ -1,13 +1,23 @@
 package com.games.chibi.assassin;
 
+import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class MainActivity extends AppCompatActivity {
+
+    DatabaseReference rootRef;
+    DatabaseReference childRef;
+    SharedPreferences localData;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        rootRef = FirebaseDatabase.getInstance().getReference();
+        localData = this.getSharedPreferences("com.games.chibi.assassin", Context.MODE_PRIVATE);
     }
 }
